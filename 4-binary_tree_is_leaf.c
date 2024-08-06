cc760eb3f6aec8619ba69c41b6764aa4339a5ec9
MrKay12 <quentin.lepoutre12@gmail.com>
2024-08-06 09:18:55 +0200

    function for cheack if node is a leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - check if node = leaf
+ *
+ *@node: pointer to node
+ *
+ *Return: if leaf 1 else 0
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+
+	return (0);
+}
